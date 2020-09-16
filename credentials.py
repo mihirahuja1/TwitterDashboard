94f8432c0b5d5c64c81d7e213d5153e4f1bc1bdb
mihirahuja1 <56941061+mihirahuja1@users.noreply.github.com>
2020-09-16 10:16:20 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
-API_KEY = "I6fZIQiiDsMKYBEbFMt0Xh2nL"
-API_SECRET_KEY = "pRXCiv4qfuoEZqM7y23Xvvh1ZbdHPzUMimxfdZq6ChyhzseRRJ"
+API_KEY = ""
+API_SECRET_KEY = ""
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-ACCESS_TOEKN = "390663782-AvhPz6EqjaVENaJjcUbHSrmqFTAdRdrxjdDNsl0O"
-ACCESS_TOKEN_SECRET = "3g7UNiRAs46ELcMF1vdN4dgPzcFv0q1xKrGztJjqsqprv"
+ACCESS_TOEKN = ""
+ACCESS_TOKEN_SECRET = ""
